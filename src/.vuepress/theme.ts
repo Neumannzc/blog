7e6c0be8a976cb69335ffc5bb8afbf09756389e8
Cafe Babe <517890027@qq.com>
2024-05-09 15:06:54 +0800

    触发git push

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -1,7 +1,6 @@
 import { hopeTheme } from "vuepress-theme-hope";
 import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
-import { MR_HOPE_AVATAR } from "./logo.js";
 
 export default hopeTheme({
   // 是否在导航栏内显示仓库链接，默认为 `true`
